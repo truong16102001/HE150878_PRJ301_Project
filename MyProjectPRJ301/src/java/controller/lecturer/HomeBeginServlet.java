6854a43f7f7955b479525bd8bfb4a9f7124be739
Truong <83231749+truong16102001@users.noreply.github.com>
2022-11-08 23:47:36 +0700

    final

--- MyProjectPRJ301/src/java/controller/lecturer/HomeBeginServlet.java
+++ MyProjectPRJ301/src/java/controller/lecturer/HomeBeginServlet.java
@@ -29,7 +29,6 @@ public class HomeBeginServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         request.getRequestDispatcher("view/login.jsp").forward(request, response);
-
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
